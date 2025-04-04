5f5c7d026d889ea04114393c7533b8204195c886
mohamed-shahien <shaheenagain@gmail.com>
2025-04-05 01:30:28 +0200

    init functions

---
+++ src/components/CoffeeForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoffeeForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CoffeeForm
---
+++ src/components/Hearo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hearo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hearo
